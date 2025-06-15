742106132e472cc680e69c4a329f526a868b796f
Reivo <200836526+reivokuuba@users.noreply.github.com>
2025-06-15 05:12:30 +0300

    Update install.php

--- authentication/install.php
+++ authentication/install.php
@@ -49,7 +49,7 @@ if (!$fortifyInstalled) {
     }
 }
 
-// **Skip fortify:install if migration file already exists**
+// **If migration file already exists, skip fortify:install**
 if ($fortifyMigrationExists) {
     echo "Skipping fortify:install. Migration file already exists.\n";
 } else {
